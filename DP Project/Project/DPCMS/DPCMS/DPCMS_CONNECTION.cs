720d5a030b72487546b2b8a86b74c393a3ec65b5
Faiqkiet <78805129+Faiqkiet@users.noreply.github.com>
2021-04-22 16:00:57 -0700

    Add files via upload

---
+++ DP Project/Project/DPCMS/DPCMS/DPCMS_CONNECTION.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DPCMS
+{
+    class DPCMS_CONNECTION
+    {
+
+        private static DPCMS_CONNECTION inst = new DPCMS_CONNECTION();
+       
+        private DPCMS_CONNECTION() { }
+
+        public static DPCMS_CONNECTION getinst(){
+            return inst;
+        }
+
+       public  SqlConnection con;
+
+        private string connection_string;
+        public void insert_Connection_string(string a)
+        {
+            connection_string = a;
+        
+        }
+        public void connect_open()
+        {
+            try
+            {
+                con = new SqlConnection(connection_string);
+                con.Open();
+                //label3.Text = "ACTIVE";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //label3.Text = "DISABLED";
+            }
+        }
+
+        public void connect_close()
+        {
+            try
+            {
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //label3.Text = "DISABLED";
+            }
+        }
+
+    }
+}
